3dc798ff75b5c7b91c261145f83dddabd0e5567a
rrmysb <rrumeysa.baltacii@gmail.com>
2026-07-10 02:23:43 +0300

    rezervasyon sistemi arayuz gorselleri eklendi

--- config/services.ts
+++ config/services.ts
@@ -20,7 +20,7 @@ export const MAYITECH_SERVICES: ServiceDetail[] = [
     ctaText: "14 Gün Ücretsiz Deneyin",
     icon: "Calendar",
     setupTime: "5 Dakika",
-    images: ["/images/rezervasyon-1.png", "/images/rezervasyon-2.png"] // Örnek görsel yolları
+    images: ["/images/shine_islem_secimi.png", "/images/shine_randevu_kayit.png"] // Örnek görsel yolları
   },
   {
     id: "kasa",
